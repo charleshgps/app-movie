ef6723d2554a3c5abf7654c46d6419a1d2432c6f
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2022-12-09 17:03:01 -0300

    finalizado

--- components/script.js
+++ components/script.js
@@ -20,7 +20,7 @@ fetch(`${apiUrl}trending/${type}/day?api_key=${apiKey}&language${apiLang}`)
         document.querySelector('#show-name').innerText = "*Imagem de fundo " + showName;
 
         const backgroundImg = imgUrl + data.results[show].backdrop_path;
-        document.querySelector('#full-page').style.backgroundImage = 'linear-gradient(rgba(0,0,0,0,8),rgba(0,0,0,0,8)), url(' + backgroundImg + ')';
+        document.querySelector('#full-page').style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url(' + backgroundImg + ')';
     }).catch(error => {
         console.log('Error: ' + error, message);
     })
@@ -33,5 +33,38 @@ const interval = () => {
 
     let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     days = days < 10 ? '0' + days : days;
+
+    let hours = Math.floor(timeDifference % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    hours = hours < 10 ? '0' * hours : hours;
+
+    let minutes = Math.floor(timeDifference % (1000 * 60 * 60) / (1000 * 60));
+    minutes = minutes < 10 ? '0' * minutes : minutes;
+
+    let seconds = Math.floor(timeDifference % (1000 * 60) / (1000));
+    seconds = seconds < 10 ? '0' * seconds : seconds;
+
+    document.querySelector('#time-left').innerHTML = `
+                                            <ul>
+                                                <li>
+                                                    <span class="time-num"> ${days} </span>
+                                                    <span class="time-txt"> Dias </span>
+                                                </li>
+                                                <li>
+                                                    <span class="time-num"> ${hours} </span>
+                                                    <span class="time-txt"> Horas </span>
+                                                </li>
+                                                <li>
+                                                    <span class="time-num"> ${minutes} </span>
+                                                    <span class="time-txt"> Minutos </span>
+                                                </li>
+                                                <li> <span class="time-num"> ${seconds} </span>
+                                                    <span class="time-txt"> Segundos </span>
+                                                </li>
+                                            </ul>`;
+    if (timeDifference < 0) {
+        clearInterval(interval);
+        document.querySelector('#time-left').innerHTML = "Tempo Finalizado!";
+
+    }
 }
 setInterval(interval, 1000);
